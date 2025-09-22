4638ffcc7e52b724aeba8cf24f6f5dea69e7e7d4
KEVIN MORENO <morenokevinfelipe@gmail.com>
2025-09-22 01:33:25 -0500

    subimos requirements

---
+++ db/client.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+# db_client = MongoClient() local
+
+
+db_client = MongoClient("mongodb+srv://AprendizajePython:12345654321@cluster01.l8zcrxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01").dbpython
+
